caa4c4c2c869c142949cd87c78e3b9c38581154d
Sharad Tripathi <y.sharad.t@gmail.com>
2020-11-13 21:55:44 +0530

    test

--- routes/shops.js
+++ routes/shops.js
@@ -63,19 +63,6 @@ var Shops = require("../models/shops");
     
 
 router.get("/", function(req, res){
-  /*var shops = [
-         {name: "2K Market", id: "abc"},
-         {name: "Dimora", id: "def"},
-         {name: "SC Juice", id: "ghi"},
-         {name: "Chat Khazana", id: "jkl"},
-         {name: "Zircon shop", id: "mno"},
-         {name: "coke station", id: "pqr"},
-         {name: "bru", id: "stu"},
-         {name: "lassi shop", id: "vwx"}
-  ]
-  res.render("shops",{shops:shops}); */
-
-
   Shops.find({}, function(err, AllShop){
     if(err){
       req.flash("error", "something went wrong");
